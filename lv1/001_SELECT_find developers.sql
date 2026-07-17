239bf860697464db51cd79e0d870161ac096eee1
rladkdud612 <ayoung6122@gmail.com>
2026-07-17 12:28:55 +0900

    Add. 이진수 비트 연산 활용

--- lv1/001_SELECT_find developers.sql
+++ lv1/001_SELECT_find developers.sql
@@ -1,7 +1,7 @@
 -- 문제: 조건에 맞는 개발자 찾기 lv.2
 -- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/276034
 -- 핵심 개념: SELECT, 비트 AND 연산(&)
--- 메모: 처음엔 JOIN + GROUP BY + HAVING으로 접근했는데, 비트 연산을 활용하면 훨씬 간단하게 해결 가능!
+-- 메모: 처음엔 JOIN + GROUP BY + HAVING으로 접근했는데, 비트 연산을 활용하면 훨씬 간단하게 해결 가능함.
 
 SELECT ID, EMAIL, FRIST_NAME, LAST_NAME
 FROM DEVELOPERS
