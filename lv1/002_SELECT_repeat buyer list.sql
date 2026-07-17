1daa26934680e648fd94e1b9e76070c4eae660ee
rladkdud612 <ayoung6122@gmail.com>
2026-07-17 12:30:17 +0900

    Add. HAVING절에서 COUNT 사용

--- lv1/002_SELECT_repeat buyer list.sql
+++ lv1/002_SELECT_repeat buyer list.sql
@@ -1,4 +1,4 @@
--- 문제: 재구매가 일어난 상품과 회원 리스트 구하기
+-- 문제: 재구매가 일어난 상품과 회원 리스트 구하기 lv.2
 -- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131536
 -- 핵심 개념: GROUP BY, HAVING, SELF JOIN
 -- 메모: GROUP BY로 한 회원이 한 상품을 두 번 이상 구매한 경우를 찾기 위해 HAVING 절에서 COUNT를 사용함, GORUP BY와 DISTINCT를 함께 사용하는 것은 지양
